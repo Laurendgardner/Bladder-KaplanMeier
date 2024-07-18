616749da533e9211e8516a4685e8d2cc2c825ee3
Laurendgardner <laurenge7@gmail.com>
2024-07-18 11:37:25 -0400

    Update KM_OS_Bladder_STATUS.R

--- KM_OS_Bladder_STATUS.R
+++ KM_OS_Bladder_STATUS.R
@@ -1,28 +1,28 @@
-#install packages
+# Install packages
 install.packages("ggsurvfit")
 install.packages("gtsummary")
 
-#load packages
+# Load packages
 library(ggsurvfit)
 library(gtsummary)
 
-#read "bladder.tsv" file into a data frame named "bladder_data"
+# Read "bladder.tsv" file into a data frame named "bladder_data"
 bladder_data <- read.table(file = "/Users/laurengardner/Projects/KP_BladderProject/bladder.tsv", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 
-#create a new column named "NewReccurranceOutcomeTable"
+# Create a new column named "NewReccurranceOutcomeTable"
 bladder_data$NewReccurrenceOutcomeTable = "NA"
 
-#assign status code for new column
+# Assign status code for new column
 bladder_data[which(bladder_data$recurrence_outcome_JD == "Yes"), "NewReccurrenceOutcomeTable"] = 1
 bladder_data[which(bladder_data$recurrence_outcome_JD == "No"), "NewReccurrenceOutcomeTable"] = 0
 
-#create a new column named "NewRecurrenceOutcomeTable_num" with numerical character values
+# Create a new column named "NewRecurrenceOutcomeTable_num" with numerical character values
 bladder_data$NewRecurrenceOutcomeTable_num <- as.numeric(as.character(bladder_data$NewReccurrenceOutcomeTable))
 
-#create a new column named "clinical_os_JD_num" with numerical character values
+# Create a new column named "clinical_os_JD_num" with numerical character values
 bladder_data$clinical_os_JD_num <- as.numeric(as.character(bladder_data$clinical_os_JD))
 
-#create Kaplan-Meier plot
+# Create Kaplan-Meier plot
 survfit2(Surv(clinical_os_JD_num, NewRecurrenceOutcomeTable_num) ~ tstage_c_JD, data = bladder_data) %>% 
   ggsurvfit() +
   labs(
@@ -32,20 +32,20 @@ survfit2(Surv(clinical_os_JD_num, NewRecurrenceOutcomeTable_num) ~ tstage_c_JD, 
   add_confidence_interval() +
   add_risktable()
 
-#estimating 1-year survival (weeks)
+# Estimating 1-year survival (weeks)
 summary(survfit(Surv(clinical_os_JD_num, NewRecurrenceOutcomeTable_num) ~ tstage_c_JD, data = bladder_data), times = 52)
 
-#table for 1-year survival time estimate
+# Table for 1-year survival time estimate
 survfit(Surv(clinical_os_JD_num, NewRecurrenceOutcomeTable_num) ~ tstage_c_JD, data = bladder_data) %>% 
   tbl_survfit(
     times = 52,
     label_header = "1-year survival (95% CI)"
   )
 
-#estimating median survival time (weeks)
+# Estimating median survival time (weeks)
 survfit(Surv(clinical_os_JD_num, NewRecurrenceOutcomeTable_num) ~ tstage_c_JD, data = bladder_data)
 
-#table for medium survival time estimate
+# Table for medium survival time estimate
 survfit(Surv(clinical_os_JD_num, NewRecurrenceOutcomeTable_num) ~ tstage_c_JD, data = bladder_data) %>% 
   tbl_survfit(
     probs = 0.5,
